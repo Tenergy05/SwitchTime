2af516bb799f918ba58010d921972f1ef4dce763
oliverfei <ofei006@uottawa.ca>
2015-10-21 10:46:50 -0400

    Updated styles with bootstrap and flatui

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,21 +1,17 @@
 var timeTick;
 var time = 0;
-var timerStarted = [false];// stores the states of all timers
+var timerStarted = [false];
 
 $(function(){
-    $("ul").sortable({ // set ul items to be sortable
-        connectWith: "ul" 
+    $("ul").sortable({
+        connectWith: "ul"
     });
     var timeStart = Date.now();
-    timeTick = setTimeout(function () { timer(timeStart) }, 1000); // start timing
+    timeTick = setTimeout(function () { timer(timeStart) }, 1000);
 });
 
-/**
- * Stops or starts a timer.
- * @param {number} tableNum - the table number indicating which timer to toggle
- */
 function toggleTime(tableNum) {
-    button = document.getElementById("s" + tableNum);
+    var button = document.getElementById("s" + tableNum);
     if (timerStarted[tableNum - 1]) {
         timerStarted[tableNum - 1] = false;
         button.innerText = "Start";
@@ -25,27 +21,15 @@ function toggleTime(tableNum) {
     }
 }
 
-/**
- * Decrements each activated timer by one second.
- * Sets a timeout for the next decrement to occur such that the timers stay accurate.
- */
 function timer(timeStart) {
     for(var i=0;i<timerStarted.length;i++)
         if (timerStarted[i])
             decTime(i+1)
-    time += 1000; // increase the time elapsed by 1000 milliseconds
-
-    // get the difference between actual and recorded elapsed times
+    time += 1000;
     var timeAdjust = Date.now() - timeStart-time;
-
-    // set the next timer tick with an adjusted delay
     setTimeout(function () { timer(timeStart) }, 1000 - timeAdjust);
 }
 
-/**
- * Decrements the time displayed for a specified table
- * @param {number} tableNum - the table number indicating which timer to decrement
- */
 function decTime(tableNum) {
     var tableHeader = document.getElementById("t"+tableNum);
     var str = tableHeader.innerText;
@@ -73,9 +57,6 @@ function decTime(tableNum) {
     tableHeader.innerText = "Table " + tableNum + " " + minStr + ":" + secStr;
 }
 
-/**
- * Create a new person and add it to the wait list.
- */
 function addPerson() {
     var listItem = document.createElement("li");
 
@@ -104,12 +85,9 @@ function addPerson() {
     waitlist.appendChild(listItem);
 }
 
-/**
- * Create a new table and add it to the list of tables.
- */
 function addTable() {
 
-    timerStarted.push(false); // store the state of a new timer
+    timerStarted.push(false);
     var tableNum = document.getElementsByClassName("header-content").length + 1;
 
     var container = document.createElement("div");
@@ -129,21 +107,24 @@ function addTable() {
     headerId.value = "t" + tableNum;
     headerContent.setAttributeNode(contentClass);
     headerContent.setAttributeNode(headerId);
-    headerContent.innerText = "Table " + tableNum + " "
-        + document.getElementById("defaultTime").value + ":00";
+    headerContent.innerText = "Table "+tableNum+" "+document.getElementById("defaultTime").value+":00";
 
     var buttons = document.createElement("div");
-    buttonClass = document.createAttribute("class");
-    buttonClass.value = "buttons";
-    buttons.setAttributeNode(buttonClass);
+    var buttonsClass = document.createAttribute("class");
+    buttonsClass.value = "buttons";
+    buttons.setAttributeNode(buttonsClass);
+    
+    var buttonClass = document.createAttribute("class");
+    buttonClass.value = "btn";
 
     var startButton = document.createElement("button");
-    sButtonId = document.createAttribute("id");
+    var sButtonId = document.createAttribute("id");
     sButtonId.value = "s"+tableNum;
-    clickAtt = document.createAttribute("onclick");
+    var clickAtt = document.createAttribute("onclick");
     clickAtt.value = "toggleTime(" + tableNum + ");";
     startButton.setAttributeNode(clickAtt);
     startButton.setAttributeNode(sButtonId);
+    startButton.setAttributeNode(buttonClass);
     startButton.innerText = "Start";
     buttons.appendChild(startButton);
 
@@ -167,28 +148,21 @@ function addTable() {
     });
 }
 
-/**
- * Remove the last table in the list of tables.
- */
 function removeTable() {
     var containers = document.getElementsByClassName("container");
     var lastTable = containers.item(containers.length - 2);
     document.body.removeChild(lastTable);
     var numTables = document.getElementsByClassName("buttons").length;
     if (numTables === 1) {
-        removeButton = document.getElementById("remove-button");
+        var removeButton = document.getElementById("remove-button");
         removeButton.disabled = true;
     }
 }
 
-/**
- * Set the value of all timers to the time specified in the "defaultTime" element.
- */
 function setTimers() {
     var timerContent = document.getElementsByClassName("header-content");
     for (var i = 0; i < timerContent.length; i++) {
         var s = timerContent.item(i).textContent;
-        timerContent.item(i).textContent = s.substring(0, s.lastIndexOf(" ") + 1)
-            + document.getElementById("defaultTime").value + ":00";
+        timerContent.item(i).textContent = s.substring(0, s.lastIndexOf(" ")+1) + document.getElementById("defaultTime").value + ":00";
     }
 }
